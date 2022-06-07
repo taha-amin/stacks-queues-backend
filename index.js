3c07f663150684a806240be2de8aa3e1848e9e40
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-07 15:28:36 -0700

    wrote for stack class in index.js

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(initialList) {
+    if (initialList) this.#list = initialList;
+  }
+
+  // methods
+
+  pushItem(item) {
+    this.#list.push(item);
+  }
+
+  removeLast(item) {
+    this.#list.pop(item);
+  }
+
+  topItem(item) {
+    this.#list.peek(item);
+  }
+
+  //   get readableList() {
+  //     return this.#list.toString();
+  //   }
+
+  //   get count() {
+  //     return this.#list.length;
+  //   }
+}
 
 class Queue {}
 
