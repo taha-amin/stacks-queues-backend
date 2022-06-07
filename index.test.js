76e6bfc257de435a25b5c94deaced4f2075e3704
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-07 16:04:19 -0700

    wrote code for pushing fox to stack

--- index.test.js
+++ index.test.js
@@ -2,8 +2,13 @@ const { Stack } = require("./index.js");
 describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
     const stack = new Stack();
-    stack.push(1);
-    expect(stack.peek()).toEqual(1);
+    stack.pushItem(1);
+    expect(stack.topItem()).toEqual(1);
   });
   // add more tests here...
+  it("#push fox to stack", () => {
+    const stack = new Stack();
+    stack.pushItem("fox");
+    expect(stack.topItem()).toEqual("fox");
+  });
 });
