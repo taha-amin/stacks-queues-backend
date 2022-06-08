e1458116e739d89056e48b7f748da32106ef0f7d
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-07 17:17:21 -0700

    tried to figure out reversing array, so close!

--- index.test.js
+++ index.test.js
@@ -11,4 +11,28 @@ describe("Stack Class", () => {
     stack.pushItem("fox");
     expect(stack.readableList).toEqual("fox");
   });
+
+  it("return an array in reverse using Stack", () => {
+    const stack = new Stack([12, 13, 14]);
+    const stackTwo = new Stack([]);
+    stack.removeLast(14);
+    stackTwo.pushItem("14");
+    stack.removeLast(13);
+    stackTwo.pushItem("13");
+    stack.removeLast(12);
+    stackTwo.pushItem("12");
+    expect(stackTwo.readableList).toEqual(["14,13,12"]);
+    // let reverseArray = (arr) => {
+    //   //Temp stack
+
+    // for (let i = 0; i < stack.length; i--) {
+    //   stack[i] = stack.removeItem();
+    // }
+
+    // for (let i = 0; i < stack.length; i++) {
+    //   stack.pushItem(stack[i]);
+    // }
+
+    expect(stack.readableList).toEqual("14,13,12");
+  });
 });
